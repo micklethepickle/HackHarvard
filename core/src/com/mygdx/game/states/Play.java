4fc75466b504ef512ca3bd8e3f65e1dd363edbf0
micklethepickle <michel_ma@hotmail.ca>
2016-10-22 11:14:44 -0400

    added a wall

--- core/src/com/mygdx/game/states/Play.java
+++ core/src/com/mygdx/game/states/Play.java
@@ -1,16 +1,41 @@
 package com.mygdx.game.states;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.handlers.GameStateManager;
 
 public class Play extends GameState{
 	
-	private BitmapFont font = new BitmapFont();
-	
+	private World world;
+	private Box2DDebugRenderer b2dr;
 	
 	public Play(GameStateManager gsm){
 		super(gsm);
-	}
+		
+		//create box2d world
+		world = new World(new Vector2(0, -5.0f), true);
+		b2dr = new Box2DDebugRenderer();
+		
+		//create seperator platform
+		BodyDef wallBDef = new BodyDef();
+		wallBDef.position.set(160, 0);
+		wallBDef.type = BodyType.StaticBody;
+		Body body = world.createBody(wallBDef);
+		
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(2, game.V_HEIGHT);
+		
+		FixtureDef fdef = new FixtureDef();
+		fdef.shape = shape;
+		body.createFixture(fdef);
+		
+		}
 
 	
 	public void handleInput(){
@@ -18,12 +43,11 @@ public class Play extends GameState{
 	}
 	public void update(float dt){
 		
+		world.step(dt, 6, 2);
+		
 	}
 	public void render(){
-		sb.setProjectionMatrix(hudcam.combined);
-		sb.begin();
-		font.draw(sb, "play state", 100, 100);
-		sb.end();
+		b2dr.render(world, hudcam.combined);
 	}
 	public void dispose(){
 		
